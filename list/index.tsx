bbd17d03e2c65fcd7ee5661d91c79365742b8d37
chris-wang15 <wlw2015@foxmail.com>
2023-04-17 21:26:11 +0800

    search button

--- list/index.tsx
+++ list/index.tsx
@@ -1,6 +1,6 @@
 import * as SQLite from 'expo-sqlite';
-import {FlatList, Switch, Text, View} from "react-native";
-import {useCallback, useEffect, useState} from "react";
+import {Button, FlatList, Switch, Text, TextInput, View} from "react-native";
+import React, {useCallback, useState} from "react";
 import styles from "./styles";
 import zonesList from "./zonesList";
 import {useFocusEffect} from "@react-navigation/native";
@@ -9,11 +9,43 @@ const ListPage = (
     props: { db: SQLite.WebSQLDatabase; }
 ) => {
     const db: SQLite.WebSQLDatabase = props.db
+    const [curName, setCurName]
+        = useState<string | undefined>(undefined);
+    const [zones, setZones]
+        = useState(zonesList)
+
+    const searchZone = () => {
+        if (curName === undefined || curName.length === 0) {
+            setZones(zonesList);
+            return
+        }
+        let filtered = zonesList.filter(
+            function (zone: { id: number, name: string }) {
+                return zone.name.includes(curName)
+            }
+        )
+        console.log("filtered: " + filtered.length);
+        setZones(filtered);
+    }
 
     return (
         <View style={styles.listContainer}>
+            <View style={styles.searchContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    value={curName}
+                    placeholder='zone'
+                    onChangeText={(str) => {
+                        // console.log("onChangeText " + str);
+                        setCurName(str);
+                    }}
+                />
+                <View style={styles.searchButton}>
+                    <Button title='Search Zone' onPress={searchZone}/>
+                </View>
+            </View>
             <FlatList
-                data={zonesList}
+                data={zones}
                 renderItem={({item}) =>
                     <ZoneItem
                         db={db}
@@ -47,7 +79,7 @@ const ZoneItem = (
                 db.transaction(tx => {
                     tx.executeSql('SELECT * FROM zones where id = ?', [index],
                         (txObj, resultSet) => {
-                            console.log(index + " / " + name + ": " + resultSet.rows.length);
+                            // console.log(index + " / " + name + ": " + resultSet.rows.length);
                             setFollowed(resultSet.rows.length > 0);
                         },
                         (txObj, error) => {
--- list/styles.ts
+++ list/styles.ts
@@ -3,6 +3,21 @@ import {StyleSheet} from "react-native";
 const styles = StyleSheet.create({
     listContainer: {
         width: '100%',
+        flexDirection: 'column'
+    },
+    searchContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 8,
+    },
+    searchInput: {
+        flex: 2,
+        marginEnd: 8,
+    },
+    searchButton: {
+        flex: 1,
+        marginEnd: 8,
     },
     zoneRow: {
         width: '95%',
